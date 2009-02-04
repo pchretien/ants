c18de3a5f19f25fc987fdfe6e868ac2912f4f7de
Philippe Chretien <philippe.chretien@gmail.com>
2009-02-04 10:02:28 -0500

    Added GNU GPL to all .py files

--- python/Source.py
+++ python/Source.py
@@ -1,12 +1,26 @@
-###############################################################################
-# Source.py
-# 
-# PHY1234, Automne 2005
-# Rapport de laboratoire 12
-# 3 decembre 2005
-# Philippe Chretien (CHRP11037001)
-# http://www.philippe-chretien.com/PHY1234/lab12.zip
-###############################################################################
+## ants ##
+#
+# This program simulates an ants colony.
+# Copyright (C) 2008,2009  Philippe Chretien
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License Version 2
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+# You will find the latest version of this code at the following address:
+# http://github.com/pchretien
+#
+# You can contact me at the following email address:
+# philippe.chretien@gmail.com
+
 
 import sys
 from math import *
